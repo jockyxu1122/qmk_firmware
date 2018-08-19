39e6aa2bbc1f5f30de58a947b0cfb315c6ffc3f6
jockyxu1122 <yx1366@nyu.edu>
2018-08-19 16:16:25 -0700

    Update keymap of rama m6-a for MAC at work.

--- keyboards/rama/m6_a/keymaps/jockyxu1122/keymap.c
+++ keyboards/rama/m6_a/keymaps/jockyxu1122/keymap.c
@@ -2,37 +2,64 @@
   Author: jockyxu1122
 
   Keymap for Rama M6-A, no LED installed.
-  Note that "Previous track" and "next track" might only work with Windows.
 */
 
 #include "../../m6_a.h"
 
-#define MEDIA_LAYER 0
-#define PAGE_LAYER 1
+enum custom_keycords {
+  LEFT_WORKSPACE,
+  MAC_OS_SLEEP,
+  MISSION_CONTROL,
+  RIGHT_WORKSPACE,
+  VIEW_ALL_WINDOWS,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch(keycode) {
+      case LEFT_WORKSPACE:
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_LEFT)SS_UP(X_LCTRL));
+        return false;
+      case MAC_OS_SLEEP:
+        SEND_STRING(
+            SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT)SS_TAP(X_POWER)SS_UP(X_LSHIFT)SS_UP(X_LCTRL));
+        return false;
+      case MISSION_CONTROL:
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_UP)SS_UP(X_LCTRL));
+        return false;
+      case RIGHT_WORKSPACE:
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_RIGHT)SS_UP(X_LCTRL));
+        return false;
+      case VIEW_ALL_WINDOWS:
+        SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_DOWN)SS_UP(X_LCTRL));
+        return false;
+    }
+  }
+  return true;
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
-  -----------------------------------------
-  |   Mo(L1)   |  Volume Up  | Play/Pause |
-  -----------------------------------------
-  | Prev track | Volume Down | Next track |
-  -----------------------------------------
+  ---------------------------------------------------------------------
+  |     MO(L1)     |         MISSON CONTROL         |   MAC OS SLEEP  |
+  ---------------------------------------------------------------------
+  | LEFT WORKSPACE | VIEW ALL WINDOWS IN CURRENT APP| RIGHT WORKSPACE |
+  ---------------------------------------------------------------------
   */
-  [MEDIA_LAYER] = KEYMAP(
-    MO(PAGE_LAYER), KC_VOLU, KC_MPLY, \
-    KC_MPRV, KC_VOLD, KC_MNXT
+  [0] = LAYOUT(
+    MO(1), MISSION_CONTROL, MAC_OS_SLEEP, \
+    LEFT_WORKSPACE, VIEW_ALL_WINDOWS, RIGHT_WORKSPACE
   ),
-
   /*
-  ------------------------
-  | TRANS |  Up  | Home  |
-  ------------------------
-  | Left  | Down | Right |
-  -----------------------
+  ------------------------------------------
+  |    TRANS    |  VOLUME UP  | PLAY/PAUSE |
+  ------------------------------------------
+  | PREV TRACK  | VOLUME Down | NEXT TRACK |
+  ------------------------------------------
   */
-  [PAGE_LAYER] = KEYMAP(
-    KC_TRNS, KC_UP, KC_HOME, \
-    KC_LEFT, KC_DOWN, KC_RIGHT
+  [1] = LAYOUT(
+    KC_TRNS, KC__VOLUP, KC_MEDIA_PLAY_PAUSE, \
+    KC_MEDIA_REWIND, KC__VOLDOWN, KC_MEDIA_FAST_FORWARD
   )
 };
 
